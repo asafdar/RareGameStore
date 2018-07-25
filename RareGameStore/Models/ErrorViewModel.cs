845221bc89bce4f59726d0753cf76b09d8d46972
Ahmed Safdar <asafdar1138@gmail.com>
2018-07-25 09:43:05 -0500

    Add project files.

---
+++ RareGameStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RareGameStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
